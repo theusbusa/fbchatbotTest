983531dc3883c4c64f5ed56d18ef08bdd79dd2d5
Theus Jasper Busa <theusbusa@gmail.com>
2020-07-06 16:42:56 +0800

    Added verification callback

--- index.js
+++ index.js
@@ -5,17 +5,17 @@
 // Imports dependencies and set up http server
 const
 	express = require('express'),
-	body_parser = require('body-parser'),
+	{ urlencoded, json } = require('body-parser'),
 	request = require('request'), // don't forget to install this! npm install request --save
 	crypto = require("crypto"), // don't forget to install this! npm install crypto --save
 	config = require("./services/config"),
-	app = express().use(body_parser.json()); // creates express http server
+	app = express(); // creates express http server
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 // Parse application/json. Verify that callback came from Facebook
-//app.use(json({ verify: verifyRequestSignature }));
+app.use(json({ verify: verifyRequestSignature }));
 
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
@@ -40,7 +40,6 @@ app.post('/webhook', (req, res) => {
 			// Check if the event is a message or postback and
 			// pass the event to the appropriate handler function
 			if (webhook_event.message) {
-				console.log(webhook_event);
 				handleMessage(sender_psid, webhook_event.message);
 			} else if (webhook_event.postback) {
 				handlePostback(sender_psid, webhook_event.postback);
@@ -171,4 +170,23 @@ function callSendAPI(sender_psid, response) {
 			console.error("Unable to send message:" + err);
 		}
 	});
+}
+
+// Verify that the callback came from Facebook.
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    console.log("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split("=");
+    var signatureHash = elements[1];
+    var expectedHash = crypto
+      .createHmac("sha1", config.appSecret)
+      .update(buf)
+      .digest("hex");
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
 }
