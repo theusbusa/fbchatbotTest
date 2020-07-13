d68756288fc0635f04087deb0341011abd9aa2f7
Theus Jasper Busa <theusbusa@gmail.com>
2020-07-13 10:45:48 +0800

    Deleted unused functions

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-//const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-
 // Imports dependencies and set up http server
 const
 	express = require('express'),
@@ -151,95 +149,6 @@ app.get('/webhook', (req, res) => {
 		}
 	}
 });
-
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
-
-	let response;
-
-	// Check if the message contains text
-	if (received_message.text) {
-	
-		// Create the payload for a basic text message
-		response = {
-			"text": `You sent the message: "${received_message.text}". Now send me an image!`
-		}
-	} else if (received_message.attachments) {
-	
-		// Gets the URL of the message attachments
-		let attachment_url =  received_message.attachments[0].payload.url;
-		response = {
-			"attachment": {
-				"type": "template",
-				"payload": {
-					"template_type": "generic",
-					"elements": [{
-						"title": "Is this the right picture?",
-						"subtitle": "Tap a button to answer.",
-						"image_url": attachment_url,
-						"buttons": [
-							{
-								"type": "postback",
-								"title": "Yes!",
-								"payload": "yes",
-							},
-							{
-								"type": "postback",
-								"title": "No!",
-								"payload": "no",
-							}
-						],
-					}]
-				}
-			}
-		}
-	}
-
-	// Sends the response message
-	callSendAPI(sender_psid, response);
-}
-
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-	let response;
-
-	// Get the  payload for the postback
-	let payload = received_postback.payload;
-
-	// Set the response based on the postback payload
-	if (payload === 'yes') {
-		response = { "text": "Thanks!" }
-	} else if (payload === 'no') {
-		response = { "text": "Oops, try sending another image." }
-	}
-	// Send the message to acknowledge the postback
-	callSendAPI(sender_psid, response);
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-	// Construct the message body
-	let request_body = {
-		"recipient": {
-			"id": sender_psid
-		},
-		"message": response
-	}
-
-	// Send the HTTP request to the Messenger Platform
-	request({
-		"uri": "https://graph.facebook.com/v2.6/me/messages",
-		"qs": { "access_token": config.pageAccesToken },
-		"method": "POST",
-		"json": request_body
-	}, (err, res, body) => {
-		if (!err) {
-			console.log('message sent!')
-		} else {
-			console.error("Unable to send message:" + err);
-		}
-	});
-}
 
 // Verify that the callback came from Facebook.
 function verifyRequestSignature(req, res, buf) {
