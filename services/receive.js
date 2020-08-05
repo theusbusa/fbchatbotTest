b75fb2e2c9154de9d8337b1710b760c36040193f
Theus Jasper Busa <theusbusa@gmail.com>
2020-08-05 14:41:31 +0800

    sending mulitple media

--- services/receive.js
+++ services/receive.js
@@ -151,7 +151,8 @@ module.exports = class Receive {
 
             console.log(result[0].answers);
             console.log(result[0].imageURL);
-            response = [ Response.genText(result[0].answers), Response.genImageTemplate(result[0].imageURL) ];
+            response = [Response.genText(result[0].answers), Response.genImageTemplate(result[0].imageURL)];
+            console.log(response);
         } else {
             response = Response.genText("I don't understand.")
         }
