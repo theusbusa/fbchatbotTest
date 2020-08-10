d1486f9014e31c3dece8f1030315c813bbfd8c24
Theus Jasper Busa <theusbusa@gmail.com>
2020-08-10 16:03:30 +0800

    displaying products

--- services/receive.js
+++ services/receive.js
@@ -110,6 +110,13 @@ module.exports = class Receive {
             const element = await dbase.mediaArray(result);
 
             response = Response.genImageTemplate2(element);
+        } else if (message.match(new RegExp(productSubcateg.join('|'), 'g')) !== null && message.match(new RegExp(["men", "women"].join('|'), 'g')) !== null) {
+            const subcateg = message.match(new RegExp(productSubcateg.join('|'), 'g'))[0].toLowerCase();
+            const gender = message.match(new RegExp(["men", "women"].join('|'), 'g'))[0].toLowerCase();
+            const result = await dbase.queryData("SELECT productName, price, imageURL, productURL FROM Products WHERE subcategory = \"" + subcateg + "\" AND gender = \"" + gender + "\"");
+            const element = await dbase.mediaArray(result);
+
+            response = Response.genImageTemplate2(element);
         } else if (message.match(new RegExp(productCateg.join('|'), 'g')) !== null) {
             const categ = message.match(new RegExp(productCateg.join('|'), 'g'))[0].toLowerCase();
             const result = await dbase.queryData("SELECT category, imageURL, gender FROM Products WHERE category = \"" + categ + "\" GROUP BY category, gender");
